80feb127779fe24334e71e7a8f95489342381e2c
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2024-12-29 20:45:24 -0500

    fibnoachi recursive soluition

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
 
-module.exports = fib;
+    let result = [0,1];
+    for(let i = 2; i<=n; i++){
+        let a  = result[i-1];
+        let b = result[i-2];
+
+        result.push(a+b);
+    }
+
+    //return result[n];
+    console.log(result.length-1);
+    //return result [result.length-1];
+    return result[n];
+
+}
+ console.log(fib(6));
+
+// Recursive Solutioin :
+
+ function fib(n){
+
+    if(n < 2){
+        return n;
+    }
+
+    return fib(n-1)+fib(n-2);
+
+ }
+
+console.log(fib(n));
+
