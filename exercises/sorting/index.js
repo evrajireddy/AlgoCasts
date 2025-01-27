f435f68308f5db1800d44b6ef144ee8dee93b8d0
Venkata Raji Reddy Eda <evrajireddy@gmail.com>
2025-01-27 11:31:22 -0500

    hash map

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,6 +1,6 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
-
+/*
 function bubbleSort(arr) {
 
     for(let i=0; i<arr.length; i++){
@@ -27,6 +27,36 @@ function mergeSort(arr) {
 
 function merge(left, right) {
 
-}
+}*/
 
-module.exports = { bubbleSort, selectionSort, mergeSort, merge };
+var myMap = new Map();
+
+    myMap.set(1, "Uno");
+    myMap.set(2, "dos");
+    myMap.set(3, "Tres");
+    myMap.set(4, "Cuarto");
+
+    console.log(myMap);
+
+    for(let key of myMap.keys()){
+
+        console.log(`key is ${key}`);
+    }
+
+    for(let value of myMap.values()){
+        console.log(`value is ${value}`);
+    }
+
+    for(let [key, value] of myMap){
+        console.log(`key is : ${key} and value is ${value}`);
+    }
+
+    myMap.forEach((value, key) => console.log(`value is ${value} and key is ${key} `));
+
+    myMap.delete(2);
+
+    console.log(myMap);
+
+    
+
+
